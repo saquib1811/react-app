d3e04034916b9bb3694def867d4c530543157b76
saquib01 <md.saquib1811@gmail.com>
2023-09-23 17:58:59 +0530

    commit

--- App.js
+++ App.js
@@ -1,12 +1,5 @@
-// Manipulate the HTML DOM using Javscript
-// const heading = document.createElement("h1");
-// heading.innerHTML = "Namaste Everyone";
-// const root = document.getElementById("root");
-// root.appendChild(heading);
-
-// Manipulate the HTML DOM using React
-
-// Create nested React Elements
+import React from "react";
+import ReactDOM from "react-dom/client"
 const heading = React.createElement(
   "h1",
   {
