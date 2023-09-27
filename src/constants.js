daa3cf5292d73add6dbad3d74849ab9a4310b17a
saquib01 <md.saquib1811@gmail.com>
2023-09-28 01:02:35 +0530

    shimmer add

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 export const imageUrl =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 export const swiggyUrl =
-  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5174627&lng=77.0773122&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5691957&lng=77.2886424&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const shimmer_card_unit = 20;
